a27b4cdff855776e38860909c582d1bede8d5604
Myeong no ah <42794561+noah2397@users.noreply.github.com>
2022-03-07 10:42:08 +0900

    middleware part2
    learn about how to use middleware, and also function as protect url

--- src/server.js
+++ src/server.js
@@ -2,20 +2,30 @@ import express from "express";
 
 const PORT = 4000;
 const app = express();
-const gossipMiddleware = (req, res, next) => {
-  console.log(`Someone is going to ${req.url}`);
+
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
   next();
 };
-
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("Allowed, you maay continue.");
+  next();
+};
 const handleHome = (req, res, next) => {
   console.log("hi");
   next();
 };
-const handleLogin = (req, res) => {
-  return res.send("Login here,.");
+const handleProtected = (req, res) => {
+  return res.send("Welcome to the private lounge");
 };
-app.get("/", gossipMiddleware, handleHome);
-app.get("/login", handleLogin);
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
