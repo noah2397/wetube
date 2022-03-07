471e0473b71994a0e2f876df6e3bd126edd89853
Myeong no ah <42794561+noah2397@users.noreply.github.com>
2022-03-07 10:22:11 +0900

    middleware part one

--- src/server.js
+++ src/server.js
@@ -1,19 +1,22 @@
 import express from "express";
 
 const PORT = 4000;
-
 const app = express();
+const gossipMiddleware = (req, res, next) => {
+  console.log(`Someone is going to ${req.url}`);
+  next();
+};
 
-const handleHome = (req, res) => {
-  return res.end();
+const handleHome = (req, res, next) => {
+  console.log("hi");
+  next();
 };
 const handleLogin = (req, res) => {
   return res.send("Login here,.");
 };
-app.get("/", handleHome);
+app.get("/", gossipMiddleware, handleHome);
 app.get("/login", handleLogin);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
-
 app.listen(PORT, handleListening);
