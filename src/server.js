47f9fa4bfbc3e86862b766ce53ebf8e72da506b4
Myeong no ah <42794561+noah2397@users.noreply.github.com>
2022-03-07 11:21:52 +0900

    External Middlewares(morgan)
    use morgan(logger middleware)

--- src/server.js
+++ src/server.js
@@ -1,31 +1,21 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
+const home = (req, res) => {
+  console.log("I will respone.");
+  return res.send("hello");
 };
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  console.log("Allowed, you maay continue.");
-  next();
+const login = (req, res) => {
+  return res.send("login");
 };
-const handleHome = (req, res, next) => {
-  console.log("hi");
-  next();
-};
-const handleProtected = (req, res) => {
-  return res.send("Welcome to the private lounge");
-};
+
 app.use(logger);
-app.use(privateMiddleware);
-app.get("/", handleHome);
-app.get("/protected", handleProtected);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
