076e7510b39e70d387c038b85193e0ff763a062a
Myeong no ah <42794561+noah2397@users.noreply.github.com>
2022-03-11 10:51:38 +0900

    separate router and server

---
+++ src/routers/userRouter.js
@@ -0,0 +1,8 @@
+import express from "express";
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+const handleDelete = (req, res) => res.send("Delete User");
+userRouter.get("/edit", handleEditUser);
+userRouter.get("/delete", handleDelete);
+export default userRouter;
