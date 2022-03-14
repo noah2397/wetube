bd824d807bb7a4d5da560c8ccbcbfb2277ea8669
Myeong no ah <42794561+noah2397@users.noreply.github.com>
2022-03-14 10:40:29 +0900

    #5.2 Partials
    PUG를 이용한 겹치는 코딩 부분 최적화 연습

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,15 +1,6 @@
-export const trending = (req, res) =>
-  res.send(
-    "<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Home</h1></body></html>"
-  );
-export const see = (req, res) => {
-  console.log(req.params);
-  return res.send(`Watch Video #${req.params.id}`);
-};
-export const edit = (req, res) => {
-  console.log(req.params);
-  return res.send("Edit");
-};
+export const trending = (req, res) => res.render("home");
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("upload");
