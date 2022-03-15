a05d07b51396e8ead026e293b4c5bcd169be6517
Myeong no ah <42794561+noah2397@users.noreply.github.com>
2022-03-15 12:08:47 +0900

    #6.2 Array Database
    생성된 videos 배열을 사용하여 값을 넣고 데이터를 포함한 mixin을 사용하여 페이지간 이동 연습

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,38 +1,38 @@
-const fakeUser = {
-  username: "Noah",
-  loggedIn: false,
-};
-
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minites age",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minites age",
+    views: 1,
+    id: 2,
+  },
+  {
+    title: "Thrid Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minites age",
+    views: 59,
+    id: 3,
+  },
+];
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "First Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minites age",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Second Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minites age",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Thrid Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minites age",
-      views: 59,
-      id: 1,
-    },
-  ];
-  return res.render("home", { pageTitle: "Home", fakeUser, videos });
+  return res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const watch = (req, res) => {
+  const { id } = req.params;
+  console.log("Show video", id);
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watch ${video.title}`, video });
+};
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("upload");
