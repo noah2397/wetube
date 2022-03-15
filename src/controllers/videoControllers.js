68b5fe6675322d0dd12081f9f77a33c84bedfd9f
Myeong no ah <42794561+noah2397@users.noreply.github.com>
2022-03-15 11:35:21 +0900

    #5.9 mixins
    mixin을 include 하고 만드는 법에 대해서 알아보았다

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -3,8 +3,35 @@ const fakeUser = {
   loggedIn: false,
 };
 
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser });
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minites age",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minites age",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Thrid Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minites age",
+      views: 59,
+      id: 1,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", fakeUser, videos });
+};
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search");
