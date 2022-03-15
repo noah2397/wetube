2d26a60fde098b02964580d21762d0d99eaac91b
Myeong no ah <42794561+noah2397@users.noreply.github.com>
2022-03-15 11:03:49 +0900

    #5.7 conditionals
    변수 할당 방법과 boolean 값을 이용한 if, else 사용 구현

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,7 +1,12 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+const fakeUser = {
+  username: "Noah",
+  loggedIn: false,
+};
+
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", fakeUser });
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
-
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("upload");
 export const deleteVideo = (req, res) => {
