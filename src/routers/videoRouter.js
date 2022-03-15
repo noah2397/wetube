45b05f4c01a95f9f335def1043ebe34d9a321d76
Myeong no ah <42794561+noah2397@users.noreply.github.com>
2022-03-15 16:12:26 +0900

    #6.3 Edit Video
    백엔드를 통하여 데이터베이스 내용 변경 실습

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,15 +1,13 @@
 import express from "express";
 import {
   watch,
-  edit,
+  getEdit,
+  postEdit,
   upload,
   deleteVideo,
 } from "../controllers/videoControllers";
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload); //upload의 순서가 중요하다
 videoRouter.get("/:id(\\d+)", watch);
-videoRouter.get("/:id(\\d+)/edit", edit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
-
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 export default videoRouter;
