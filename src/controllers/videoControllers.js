00a3274068a3b9e1cf4096ac8c08337bb69fe6ac
Myeong no ah <42794561+noah2397@users.noreply.github.com>
2022-03-16 20:19:29 +0900

    #6.6 More Practice
    영상 추가하기 함수를 선언해 복습

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -44,3 +44,19 @@ export const postEdit = (req, res) => {
 
   return res.redirect(`/videos/${id}`);
 };
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+export const postUpload = (req, res) => {
+  const newVideo = {
+    title: req.body.title,
+    rating: 0,
+    comments: 0,
+    createdAt: "just now",
+    views: 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+  console.log(req.body);
+  return res.redirect("/");
+};
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,11 +3,12 @@ import {
   watch,
   getEdit,
   postEdit,
-  upload,
-  deleteVideo,
+  getUpload,
+  postUpload,
 } from "../controllers/videoControllers";
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 export default videoRouter;
