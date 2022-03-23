1602523773ba9bcd5645dba31ea5e5d38741180b
Myeong no ah <42794561+noah2397@users.noreply.github.com>
2022-03-23 21:51:49 +0900

    #7.3 Confirmation form
    User 모델 생성 후 user를 만드는 폼 생성, or연산자와 user.exist를 활용한 확인 조건 생성 연습

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,4 +1,30 @@
-export const join = (req, res) => res.send("join");
+import User from "../models/User";
+
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+export const postJoin = async (req, res) => {
+  const { name, username, email, password, password2, location } = req.body;
+  if (password !== password2) {
+    return res.render("join", {
+      pageTitle: "Join",
+      errorMessage: "Password confimation does not match.",
+    });
+  }
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.render("join", {
+      pageTitle: "Join",
+      errorMessage: "This username/email is already taken.",
+    });
+  }
+  await User.create({
+    name,
+    username,
+    email,
+    password,
+    location,
+  });
+  res.redirect("/login");
+};
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const login = (req, res) => res.send("Login");
