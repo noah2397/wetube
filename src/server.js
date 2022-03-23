c4938d28049aff77608aee924b9260288b871313
Myeong no ah <42794561+noah2397@users.noreply.github.com>
2022-03-23 22:26:08 +0900

    #7.6 Login Part
    bcrypt의 hash기능을 활용한 비밀번호 대조 및 로그인 기능 구현 실습

--- src/server.js
+++ src/server.js
@@ -5,11 +5,11 @@ import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
 const app = express();
-//const logger = morgan("dev");
+const logger = morgan("dev");
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
-//app.use(logger);
+app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
