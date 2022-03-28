e03ad018eb1f7943208dc5d2d210af3b86981c09
Myeong no ah <42794561+noah2397@users.noreply.github.com>
2022-03-28 20:14:35 +0900

    #7.19 Github Login
    access_token을 가져와 user의 정보 출력 성공

--- src/middleware.js
+++ src/middleware.js
@@ -3,6 +3,6 @@ export const localsMiddleware = (req, res, next) => {
 
   res.locals.siteName = "Wetube";
   res.locals.loggedInUser = req.session.user;
-  console.log(res.locals);
+  //sconsole.log(res.locals);
   next();
 };
