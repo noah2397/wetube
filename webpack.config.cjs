355dc2aaa99fb78ef94e21718e3bcd9274361561
Myeong no ah <42794561+noah2397@users.noreply.github.com>
2022-04-01 13:28:11 +0900

    #9.4 SCSS loader
    style-loader, css-loader, sass-loader를 들여와서 scss 사용

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,7 +1,9 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/client/js/main.js",
+  plugins: [new MiniCssExtractPlugin()],
   mode: `development`,
   output: {
     filename: "main.js",
@@ -11,10 +13,16 @@ module.exports = {
     rules: [
       {
         test: /\.js$/, //모든 js파일을 가져다가 몇가지 변환을 시키고 싶다
-        loader: "babel-loader",
-        options: {
-          presets: [["@babel/preset-env", { targets: "defaults" }]],
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
         },
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"], //webpack은 뒤에서부터 시작한다
       },
     ],
   },
