bf287e279a9ff238220a2ab2778e7023cbd29310
Myeong no ah <42794561+noah2397@users.noreply.github.com>
2022-04-01 17:29:01 +0900

    #11.3 make video util
    멈춤, 재생, 뮤트, 볼륨 조절 구현

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,7 +2,10 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: "./src/client/js/main.js",
+  entry: {
+    main: "./src/client/js/main.js",
+    videoPlayer: "./src/client/js/videoPlayer.js",
+  },
   mode: `development`,
   watch: true, //내용이 업데이트 될때마다 즉석반영함
   plugins: [
@@ -11,7 +14,7 @@ module.exports = {
     }),
   ],
   output: {
-    filename: "js/main.js",
+    filename: "js/[name].js",
     path: path.resolve(__dirname, "assets"),
     clean: true,
   },
